f4a438f15a73a1c04019f4ea974c979723df90b0
Mrunbearable <stephen.wirianata@education.nsw.gov.au>
2025-11-06 15:06:46 +1100

    done task 2

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('service-worker.js')
+            .then(registration => {
+                console.log('ServiceWorker registered!', registration);
+            })
+            .catch(error => {
+                console.log('ServiceWorker registration failed:', error);
+            });
+    });
+}
+
 let clicks = 0;
 const button = document.getElementById('clickMe');
 const display = document.getElementById('clickCount');
@@ -14,5 +27,25 @@ reset_button.addEventListener('click', () => {
     display.textContent = `Button clicks: ${clicks}`;
     display_reset.textContent = 'counter has been reset';
 });
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (event) => {
+    // Prevent Chrome from automatically showing prompt
+    event.preventDefault();
+    
+    // Show install button
+    const installBtn = document.getElementById('installBtn');
+    installBtn.style.display = 'block';
+    
+    installBtn.addEventListener('click', () => {
+        // Show install prompt
+        event.prompt();
+        
+        // Hide install button
+        installBtn.style.display = 'none';
+    });
+});
+
 
 
