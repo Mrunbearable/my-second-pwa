839633f95429019d669c2a59025445f6a7f77715
Mrunbearable <stephen.wirianata@education.nsw.gov.au>
2025-11-06 14:31:53 +1100

    done

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 let clicks = 0;
 const button = document.getElementById('clickMe');
 const display = document.getElementById('clickCount');
+const reset_button = document.getElementById('clickReset')
+const display_reset = document.getElementById('clickResetting');
 
 button.addEventListener('click', () => {
     clicks++;
     display.textContent = `Button clicks: ${clicks}`;
 });
+
+reset_button.addEventListener('click', () => {
+    clicks = 0;
+    display.textContent = `Button clicks: ${clicks}`;
+    display_reset.textContent = 'counter has been reset';
+});
+
+
